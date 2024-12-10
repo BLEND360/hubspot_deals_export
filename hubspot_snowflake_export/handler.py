d4f463976f9d1a8597fee6a4fc537c894971d3a9
krishna-blend <krishna.undamatla@blend360.com>
2024-12-10 16:01:01 +0530

    added last sync update query

--- hubspot_snowflake_export/handler.py
+++ hubspot_snowflake_export/handler.py
@@ -1,7 +1,8 @@
 import traceback
+from datetime import datetime, timezone
 
 from .events import single_deal_fetch, bulk_deals_fetch, back_fill_deals, schedule_fetch
-from .utils.config import SF_WAREHOUSE, SF_DATABASE, SF_SCHEMA, SF_ROLE
+from .utils.config import SF_WAREHOUSE, SF_DATABASE, SF_SCHEMA, SF_ROLE, SF_SYNC_INFO_TABLE
 from .utils.snowflake_db import create_sf_connection, close_sf_connection
 
 
@@ -10,6 +11,9 @@ def lambda_handler(event, context):
     print(f"Received Event: {event_job}")
     sf_conn = create_sf_connection(SF_WAREHOUSE, SF_DATABASE, SF_SCHEMA, SF_ROLE)
     sf_cursor = sf_conn.cursor()
+
+    curr_time = datetime.now(timezone.utc)
+    formatted_datetime = curr_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
     try:
 
@@ -28,10 +32,44 @@ def lambda_handler(event, context):
         else:
             print(f"Invalid event: {event_job}")
 
+        sync_update_sql = f"""
+            MERGE INTO {SF_SYNC_INFO_TABLE} AS target
+            USING (VALUES 
+                ('DEALS', '{formatted_datetime}', 'System', '{event_job.upper()}', 'SUCCESS', NULL)
+            ) AS source (ENTITY_NAME, LAST_UPDATED_ON, UPDATED_BY, UPDATE_EVENT, LAST_SYNC_STATUS, LAST_FAILED_ON)
+            ON target.ENTITY_NAME = source.ENTITY_NAME
+            WHEN MATCHED THEN
+                UPDATE SET target.LAST_UPDATED_ON = source.LAST_UPDATED_ON, 
+                target.UPDATED_BY = source.UPDATED_BY,
+                target.UPDATE_EVENT = source.UPDATE_EVENT,
+                target.LAST_SYNC_STATUS = source.LAST_SYNC_STATUS
+            WHEN NOT MATCHED THEN
+                INSERT (ENTITY_NAME, LAST_UPDATED_ON, UPDATED_BY, UPDATE_EVENT, LAST_SYNC_STATUS, LAST_FAILED_ON)
+                VALUES (source.ENTITY_NAME, source.LAST_UPDATED_ON, source.UPDATED_BY, source.UPDATE_EVENT, source.LAST_SYNC_STATUS, source.LAST_FAILED_ON);
+            """
+        sf_cursor.execute(sync_update_sql)
+
         close_sf_connection(sf_conn)
 
     except Exception as ex:
         traceback.print_exc()
+        sync_failed_sql = f"""
+            MERGE INTO {SF_SYNC_INFO_TABLE} AS target
+            USING (VALUES 
+                ('DEALS', '{formatted_datetime}', 'System', '{event_job.upper()}', 'FAILED', '{formatted_datetime}')
+            ) AS source (ENTITY_NAME, LAST_UPDATED_ON, UPDATED_BY, UPDATE_EVENT, LAST_SYNC_STATUS, LAST_FAILED_ON)
+            ON target.ENTITY_NAME = source.ENTITY_NAME
+            WHEN MATCHED THEN
+                UPDATE SET target.LAST_UPDATED_ON = source.LAST_UPDATED_ON, 
+                target.UPDATED_BY = source.UPDATED_BY,
+                target.UPDATE_EVENT = source.UPDATE_EVENT,
+                target.LAST_SYNC_STATUS = source.LAST_SYNC_STATUS,
+                target.LAST_FAILED_ON = source.LAST_FAILED_ON
+            WHEN NOT MATCHED THEN
+                INSERT (ENTITY_NAME, LAST_UPDATED_ON, UPDATED_BY, UPDATE_EVENT, LAST_SYNC_STATUS, LAST_FAILED_ON)
+                VALUES (source.ENTITY_NAME, source.LAST_UPDATED_ON, source.UPDATED_BY, source.UPDATE_EVENT, source.LAST_SYNC_STATUS, source.LAST_FAILED_ON);
+            """
+        sf_cursor.execute(sync_failed_sql)
         close_sf_connection(sf_conn)
 
     return "success"
